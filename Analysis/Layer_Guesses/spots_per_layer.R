de80bd1a8be47f79cc851b83ca658415924a1a74
lcolladotor <lcollado@jhsph.edu>
2020-01-16 14:41:22 -0500

    Update spots per layer code now that I know that rounds 1 and 2 are disjoint. 352 spots across all 12 images are missing layer guesses. This data is saved in Analysis/Layer_Guesses/layer_guess_tab.Rdata

--- Analysis/Layer_Guesses/spots_per_layer.R
+++ Analysis/Layer_Guesses/spots_per_layer.R
@@ -9,8 +9,24 @@ load(here(
     'Human_DLPFC_Visium_processedData_sce_scran.Rdata'
 ))
 
-
+## Merge the first and second round merged guesses
 tab <-
+    read.csv(
+        dir(
+            here('Analysis', 'Layer_Guesses', 'First_Round'),
+            pattern = 'Merged',
+            full.names = TRUE
+        ),
+        header = TRUE,
+        stringsAsFactors = FALSE,
+        na.strings = ''
+    )
+
+tab$key <- paste0(tab$sample_name,
+    '_',
+    tab$spot_name)
+    
+tab2 <-
     read.csv(
         dir(
             here('Analysis', 'Layer_Guesses', 'Second_Round'),
@@ -26,16 +42,27 @@ tab$key <- paste0(tab$sample_name,
     '_',
     tab$spot_name)
 
+tab2$key <- paste0(tab2$sample_name,
+    '_',
+    tab2$spot_name)
 
-## From below, the guesses are for samples 3,4 + 7,8 + 11,12
-## (the ones 300 microns away)
-unique(sce$sample_name)
-# [1] 151507 151508 151509 151510 151669 151670 151671 151672 151673 151674 151675 151676
-unique(tab$sample_name)
-# [1] 151509 151510 151671 151672 151675 151676
+stopifnot(!any(tab$key %in% tab2$key))
+stopifnot(!any(tab2$key %in% tab$key))
 
+## Save for other work
+layer_guess_tab <- rbind(tab, tab2)
+rownames(layer_guess_tab) <- NULL
+save(layer_guess_tab, file = here('Analysis', 'Layer_Guesses', 'layer_guess_tab.Rdata'))
+
+## Find which ones are missing:
+m <- match(sce$key, layer_guess_tab$key)
+options(width = 100)
+addmargins(table(sce$sample_name[is.na(m)]))
+# 151507 151508 151509 151510 151669 151670 151671 151672 151673 151674 151675 151676    Sum
+#      5      3      1     39     25     14     17    127     28     38     26     29    352
+     
 ## Spots per layer
-with(tab, tapply(layer, sample_name, table))
+with(layer_guess_tab, tapply(layer, sample_name, table))
 
 ## tidyverse-way
 spots_layer <-
@@ -49,65 +76,64 @@ spots_layer$subject <-
 save(spots_layer,
     file = here('Analysis', 'Layer_Guesses', 'spots_layer.Rdata'))
 
-## Global mean: 641.1 spots per layer across all 6 slides
+## Global mean: 604.4 spots per layer across all slides
 ## and across all layers
 summary(spots_layer$spots_per_layer)
-# Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
-# 166.0   292.8   533.0   641.1   753.0  1918.0
+#  Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 200.0   297.5   498.0   604.4   687.8  2175.0
 
-## Mean number of spots per layer for each of the 6 slides
+## Mean number of spots per layer for each of the 12 slides
 summarize(spots_layer, mean_spots = mean(spots_per_layer))
 # # A tibble: 6 x 2
-# sample_name mean_spots
-# <int>      <dbl>
-#     1      151509       684
-# 2      151510       656.
-# 3      151671       819.
-# 4      151672       778.
-# 5      151675       509.
-# 6      151676       490.
+#   sample_name mean_spots
+#         <int>      <dbl>
+# 1      151507       603
+# 2      151508       626.
+# 3      151669       727.
+# 4      151670       697.
+# 5      151673       516.
+# 6      151674       519.
 
 ## Mean number of spots per layer for each of the different layers
-## across all 6 images
+## across all 12 images
 group_by(spots_layer, layer) %>% summarize(mean_spots = mean(spots_per_layer))
 # # A tibble: 8 x 2
-# layer     mean_spots
-# <chr>          <dbl>
-#     1 Layer 1         746.
-# 2 Layer 2         445.
-# 3 Layer 2/3      1746.
-# 4 Layer 3        1316.
-# 5 Layer 4         294.
-# 6 Layer 5         584.
-# 7 Layer 6         531.
-# 8 WM              397.
+#   layer     mean_spots
+#   <chr>          <dbl>
+# 1 Layer 1         584
+# 2 Layer 2         269.
+# 3 Layer 2/3      2158
+# 4 Layer 3        1128.
+# 5 Layer 4         297
+# 6 Layer 5         633.
+# 7 Layer 6         503.
+# 8 WM              355.
 
 ## Mean number of spots per layer across each of the 3 subjects
-## (so data from 2 slides for each subject)
 group_by(spots_layer, subject, layer) %>% summarize(mean_spots = mean(spots_per_layer))
 # # A tibble: 19 x 3
 # # Groups:   subject [3]
-# subject layer     mean_spots
-# <chr>   <chr>          <dbl>
-#     1 Br5292  Layer 1        1184.
-# 2 Br5292  Layer 2         626
-# 3 Br5292  Layer 3        1829
-# 4 Br5292  Layer 4         344.
-# 5 Br5292  Layer 5         336.
-# 6 Br5292  Layer 6         197
-# 7 Br5292  WM              175
-# 8 Br5595  Layer 2/3      1746.
-# 9 Br5595  Layer 4         274.
-# 10 Br5595  Layer 5         724.
-# 11 Br5595  Layer 6         821
-# 12 Br5595  WM              424
-# 13 Br8100  Layer 1         308.
-# 14 Br8100  Layer 2         264.
-# 15 Br8100  Layer 3         804.
-# 16 Br8100  Layer 4         264.
-# 17 Br8100  Layer 5         690.
-# 18 Br8100  Layer 6         574.
-# 19 Br8100  WM              592.
+#    subject layer     mean_spots
+#    <chr>   <chr>          <dbl>
+#  1 Br5292  Layer 1         842.
+#  2 Br5292  Layer 2         300
+#  3 Br5292  Layer 3        1300
+#  4 Br5292  Layer 4         371
+#  5 Br5292  Layer 5         706
+#  6 Br5292  Layer 6         506.
+#  7 Br5292  WM              277
+#  8 Br5595  Layer 2/3      2158
+#  9 Br5595  Layer 4         288.
+# 10 Br5595  Layer 5         546.
+# 11 Br5595  Layer 6         350.
+# 12 Br5595  WM              220.
+# 13 Br8100  Layer 1         326.
+# 14 Br8100  Layer 2         238.
+# 15 Br8100  Layer 3         956.
+# 16 Br8100  Layer 4         232.
+# 17 Br8100  Layer 5         647
+# 18 Br8100  Layer 6         653
+# 19 Br8100  WM              569
 
 ## Also check the number of cells per spot
 
@@ -119,27 +145,82 @@ summary(sce$cell_count)
 ## Then by sample
 group_by(as.data.frame(colData(sce)), sample_name) %>% summarize(mean_cells = mean(cell_count))
 # # A tibble: 12 x 2
-# sample_name mean_cells
-# <int>      <dbl>
-#     1      151507       2.20
-# 2      151508       3.31
-# 3      151509       2.99
-# 4      151510       3.20
-# 5      151669       2.63
-# 6      151670       5.59
-# 7      151671       2.73
-# 8      151672       2.07
-# 9      151673       4.53
+#    sample_name mean_cells
+#          <int>      <dbl>
+#  1      151507       2.20
+#  2      151508       3.31
+#  3      151509       2.99
+#  4      151510       3.20
+#  5      151669       2.63
+#  6      151670       5.59
+#  7      151671       2.73
+#  8      151672       2.07
+#  9      151673       4.53
 # 10      151674       3.97
 # 11      151675       3.46
 # 12      151676       3.24
 
+layer_guess_tab$cell_count = sce$cell_count[match(layer_guess_tab$key, sce$key)]
+
+group_by(layer_guess_tab, layer) %>% 
+	summarize(prop0 = mean(cell_count == 0),
+		prop1 = mean(cell_count == 1))
+# # A tibble: 8 x 3
+#   layer      prop0  prop1
+#   <chr>      <dbl>  <dbl>
+# 1 Layer 1   0.334  0.217
+# 2 Layer 2   0.142  0.179
+# 3 Layer 2/3 0.0945 0.189
+# 4 Layer 3   0.112  0.166
+# 5 Layer 4   0.0313 0.118
+# 6 Layer 5   0.0344 0.119
+# 7 Layer 6   0.0484 0.128
+# 8 WM        0.0385 0.0594
+
+group_by(layer_guess_tab, sample_name) %>% 
+	summarize(prop0 = mean(cell_count == 0),
+		prop1 = mean(cell_count == 1))
+# # A tibble: 12 x 3
+#    sample_name  prop0  prop1
+#          <int>  <dbl>  <dbl>
+#  1      151507 0.243  0.201
+#  2      151508 0.132  0.139
+#  3      151509 0.216  0.132
+#  4      151510 0.101  0.133
+#  5      151669 0.0589 0.176
+#  6      151670 0.0227 0.0672
+#  7      151671 0.0584 0.164
+#  8      151672 0.120  0.263
+#  9      151673 0.0233 0.0584
+# 10      151674 0.0413 0.114
+# 11      151675 0.0645 0.160
+# 12      151676 0.0851 0.187
+
+group_by(layer_guess_tab, layer, sample_name) %>% 
+	summarize(prop0 = mean(cell_count == 0),
+		prop1 = mean(cell_count == 1))
+# # A tibble: 76 x 4
+# # Groups:   layer [8]
+#    layer   sample_name prop0 prop1
+#    <chr>         <int> <dbl> <dbl>
+#  1 Layer 1      151507 0.704 0.165
+#  2 Layer 1      151508 0.486 0.260
+#  3 Layer 1      151509 0.168 0.197
+#  4 Layer 1      151510 0.312 0.261
+#  5 Layer 1      151673 0.143 0.172
+#  6 Layer 1      151674 0.166 0.192
+#  7 Layer 1      151675 0.134 0.186
+#  8 Layer 1      151676 0.242 0.249
+#  9 Layer 2      151507 0.364 0.305
+# 10 Layer 2      151508 0.163 0.224
+# # … with 66 more rows
+		
 ## Next by subject
 group_by(as.data.frame(colData(sce)), subject) %>% summarize(mean_cells = mean(cell_count))
 # # A tibble: 3 x 2
-# subject mean_cells
-# <chr>        <dbl>
-#     1 Br5292        2.94
+#   subject mean_cells
+#   <chr>        <dbl>
+# 1 Br5292        2.94
 # 2 Br5595        3.19
 # 3 Br8100        3.81
 
@@ -147,9 +228,9 @@ group_by(as.data.frame(colData(sce)), subject) %>% summarize(mean_cells = mean(c
 group_by(as.data.frame(colData(sce)), subject, position) %>% summarize(mean_cells = mean(cell_count))
 # # A tibble: 6 x 3
 # # Groups:   subject [3]
-# subject position mean_cells
-# <chr>   <chr>         <dbl>
-#     1 Br5292  0              2.77
+#   subject position mean_cells
+#   <chr>   <chr>         <dbl>
+# 1 Br5292  0              2.77
 # 2 Br5292  300            3.10
 # 3 Br5595  0              4.08
 # 4 Br5595  300            2.40
@@ -164,62 +245,79 @@ options(width = 120)
 session_info()
 
 # ─ Session info ───────────────────────────────────────────────────────────────────────────────────────────────────────
-# setting  value
-# version  R version 3.6.2 (2019-12-12)
-# os       Windows 10 x64
-# system   x86_64, mingw32
-# ui       RStudio
-# language (EN)
-# collate  English_United States.1252
-# ctype    English_United States.1252
-# tz       America/Mexico_City
-# date     2020-01-07
+#  setting  value
+#  version  R version 3.6.1 Patched (2019-10-31 r77350)
+#  os       CentOS Linux 7 (Core)
+#  system   x86_64, linux-gnu
+#  ui       X11
+#  language (EN)
+#  collate  en_US.UTF-8
+#  ctype    en_US.UTF-8
+#  tz       US/Eastern
+#  date     2020-01-16
 #
 # ─ Packages ───────────────────────────────────────────────────────────────────────────────────────────────────────────
-# package              * version   date       lib source
-# assertthat             0.2.1     2019-03-21 [1] CRAN (R 3.6.1)
-# backports              1.1.5     2019-10-02 [1] CRAN (R 3.6.1)
-# Biobase              * 2.46.0    2019-10-29 [1] Bioconductor
-# BiocGenerics         * 0.32.0    2019-10-29 [1] Bioconductor
-# BiocParallel         * 1.20.1    2019-12-21 [1] Bioconductor
-# bitops                 1.0-6     2013-08-17 [1] CRAN (R 3.6.0)
-# cli                    2.0.0     2019-12-09 [1] CRAN (R 3.6.1)
-# crayon                 1.3.4     2017-09-16 [1] CRAN (R 3.6.1)
-# DelayedArray         * 0.12.1    2019-12-17 [1] Bioconductor
-# dplyr                * 0.8.3     2019-07-04 [1] CRAN (R 3.6.1)
-# fansi                  0.4.0     2018-10-05 [1] CRAN (R 3.6.1)
-# GenomeInfoDb         * 1.22.0    2019-10-29 [1] Bioconductor
-# GenomeInfoDbData       1.2.2     2019-12-18 [1] Bioconductor
-# GenomicRanges        * 1.38.0    2019-10-29 [1] Bioconductor
-# glue                   1.3.1     2019-03-12 [1] CRAN (R 3.6.1)
-# here                 * 0.1       2017-05-28 [1] CRAN (R 3.6.1)
-# IRanges              * 2.20.1    2019-11-20 [1] Bioconductor
-# lattice                0.20-38   2018-11-04 [2] CRAN (R 3.6.2)
-# magrittr               1.5       2014-11-22 [1] CRAN (R 3.6.1)
-# Matrix                 1.2-18    2019-11-27 [2] CRAN (R 3.6.2)
-# matrixStats          * 0.55.0    2019-09-07 [1] CRAN (R 3.6.1)
-# packrat                0.5.0     2018-11-14 [1] CRAN (R 3.6.1)
-# pillar                 1.4.3     2019-12-20 [1] CRAN (R 3.6.2)
-# pkgconfig              2.0.3     2019-09-22 [1] CRAN (R 3.6.1)
-# purrr                  0.3.3     2019-10-18 [1] CRAN (R 3.6.1)
-# R6                     2.4.1     2019-11-12 [1] CRAN (R 3.6.1)
-# Rcpp                   1.0.3     2019-11-08 [1] CRAN (R 3.6.1)
-# RCurl                  1.95-4.12 2019-03-04 [1] CRAN (R 3.6.0)
-# rlang                  0.4.2     2019-11-23 [1] CRAN (R 3.6.1)
-# rprojroot              1.3-2     2018-01-03 [1] CRAN (R 3.6.1)
-# rstudioapi             0.10      2019-03-19 [1] CRAN (R 3.6.1)
-# S4Vectors            * 0.24.1    2019-12-01 [1] Bioconductor
-# sessioninfo          * 1.1.1     2018-11-05 [1] CRAN (R 3.6.1)
-# SingleCellExperiment * 1.8.0     2019-10-29 [1] Bioconductor
-# SummarizedExperiment * 1.16.1    2019-12-20 [1] Bioconductor
-# tibble                 2.1.3     2019-06-06 [1] CRAN (R 3.6.1)
-# tidyselect             0.2.5     2018-10-11 [1] CRAN (R 3.6.1)
-# utf8                   1.1.4     2018-05-24 [1] CRAN (R 3.6.1)
-# vctrs                  0.2.1     2019-12-17 [1] CRAN (R 3.6.2)
-# withr                  2.1.2     2018-03-15 [1] CRAN (R 3.6.1)
-# XVector                0.26.0    2019-10-29 [1] Bioconductor
-# zeallot                0.1.0     2018-01-28 [1] CRAN (R 3.6.1)
-# zlibbioc               1.32.0    2019-10-29 [1] Bioconductor
+#  package              * version   date       lib source
+#  assertthat             0.2.1     2019-03-21 [2] CRAN (R 3.6.1)
+#  backports              1.1.5     2019-10-02 [1] CRAN (R 3.6.1)
+#  Biobase              * 2.46.0    2019-10-29 [2] Bioconductor
+#  BiocGenerics         * 0.32.0    2019-10-29 [1] Bioconductor
+#  BiocParallel         * 1.20.1    2019-12-21 [1] Bioconductor
+#  bitops                 1.0-6     2013-08-17 [2] CRAN (R 3.6.1)
+#  cli                    2.0.0     2019-12-09 [1] CRAN (R 3.6.1)
+#  colorout             * 1.2-2     2019-10-31 [1] Github (jalvesaq/colorout@641ed38)
+#  colorspace             1.4-1     2019-03-18 [2] CRAN (R 3.6.1)
+#  crayon                 1.3.4     2017-09-16 [1] CRAN (R 3.6.1)
+#  DelayedArray         * 0.12.0    2019-10-29 [2] Bioconductor
+#  digest                 0.6.23    2019-11-23 [1] CRAN (R 3.6.1)
+#  dplyr                * 0.8.3     2019-07-04 [1] CRAN (R 3.6.1)
+#  fansi                  0.4.0     2018-10-05 [1] CRAN (R 3.6.1)
+#  GenomeInfoDb         * 1.22.0    2019-10-29 [1] Bioconductor
+#  GenomeInfoDbData       1.2.2     2019-10-28 [2] Bioconductor
+#  GenomicRanges        * 1.38.0    2019-10-29 [1] Bioconductor
+#  ggplot2                3.2.1     2019-08-10 [1] CRAN (R 3.6.1)
+#  glue                   1.3.1     2019-03-12 [1] CRAN (R 3.6.1)
+#  gtable                 0.3.0     2019-03-25 [2] CRAN (R 3.6.1)
+#  here                 * 0.1       2017-05-28 [1] CRAN (R 3.6.1)
+#  htmltools              0.4.0     2019-10-04 [1] CRAN (R 3.6.1)
+#  htmlwidgets            1.5.1     2019-10-08 [1] CRAN (R 3.6.1)
+#  httpuv                 1.5.2     2019-09-11 [1] CRAN (R 3.6.1)
+#  IRanges              * 2.20.1    2019-11-20 [1] Bioconductor
+#  jsonlite               1.6       2018-12-07 [2] CRAN (R 3.6.1)
+#  later                  1.0.0     2019-10-04 [1] CRAN (R 3.6.1)
+#  lattice                0.20-38   2018-11-04 [3] CRAN (R 3.6.1)
+#  lazyeval               0.2.2     2019-03-15 [2] CRAN (R 3.6.1)
+#  magrittr               1.5       2014-11-22 [1] CRAN (R 3.6.1)
+#  Matrix                 1.2-17    2019-03-22 [3] CRAN (R 3.6.1)
+#  matrixStats          * 0.55.0    2019-09-07 [1] CRAN (R 3.6.1)
+#  munsell                0.5.0     2018-06-12 [2] CRAN (R 3.6.1)
+#  pillar                 1.4.3     2019-12-20 [1] CRAN (R 3.6.1)
+#  pkgconfig              2.0.3     2019-09-22 [1] CRAN (R 3.6.1)
+#  png                    0.1-7     2013-12-03 [2] CRAN (R 3.6.1)
+#  promises               1.1.0     2019-10-04 [1] CRAN (R 3.6.1)
+#  purrr                  0.3.3     2019-10-18 [2] CRAN (R 3.6.1)
+#  R6                     2.4.0     2019-02-14 [2] CRAN (R 3.6.1)
+#  Rcpp                   1.0.3     2019-11-08 [1] CRAN (R 3.6.1)
+#  RCurl                  1.95-4.12 2019-03-04 [2] CRAN (R 3.6.1)
+#  rlang                  0.4.2     2019-11-23 [1] CRAN (R 3.6.1)
+#  rmote                * 0.3.4     2019-10-31 [1] Github (cloudyr/rmote@fbce611)
+#  rprojroot              1.3-2     2018-01-03 [2] CRAN (R 3.6.1)
+#  S4Vectors            * 0.24.1    2019-12-01 [1] Bioconductor
+#  scales                 1.0.0     2018-08-09 [2] CRAN (R 3.6.1)
+#  servr                  0.15      2019-08-07 [1] CRAN (R 3.6.1)
+#  sessioninfo          * 1.1.1     2018-11-05 [1] CRAN (R 3.6.1)
+#  SingleCellExperiment * 1.8.0     2019-10-29 [2] Bioconductor
+#  SummarizedExperiment * 1.16.1    2019-12-19 [1] Bioconductor
+#  tibble                 2.1.3     2019-06-06 [1] CRAN (R 3.6.1)
+#  tidyselect             0.2.5     2018-10-11 [2] CRAN (R 3.6.1)
+#  utf8                   1.1.4     2018-05-24 [1] CRAN (R 3.6.1)
+#  vctrs                  0.2.1     2019-12-17 [1] CRAN (R 3.6.1)
+#  withr                  2.1.2     2018-03-15 [2] CRAN (R 3.6.1)
+#  xfun                   0.11      2019-11-12 [1] CRAN (R 3.6.1)
+#  XVector                0.26.0    2019-10-29 [1] Bioconductor
+#  zeallot                0.1.0     2018-01-28 [1] CRAN (R 3.6.1)
+#  zlibbioc               1.32.0    2019-10-29 [2] Bioconductor
 #
-# [1] D:/Documents/R/win-library/3.6
-# [2] D:/R/R-3.6.2/library
+# [1] /users/lcollado/R/3.6.x
+# [2] /jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/svnR-3.6.x/R/3.6.x/lib64/R/site-library
+# [3] /jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/svnR-3.6.x/R/3.6.x/lib64/R/library
